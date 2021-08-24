a7bff46326e9873d922cc7c394868fbed55c2f29
danorris709 <dnorris443@gmail.com>
2021-08-24 18:08:11 +0100

    Feat: added main command

---
+++ forge/src/main/java/com/envyful/menus/forge/command/MenuCommand.java
@@ -0,0 +1,21 @@
+package com.envyful.menus.forge.command;
+
+import com.envyful.api.command.annotate.Command;
+import com.envyful.api.command.annotate.Permissible;
+import com.envyful.api.command.annotate.executor.CommandProcessor;
+import com.envyful.api.command.annotate.executor.Sender;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.util.text.TextComponentString;
+
+@Command(
+        value = "menus",
+        description = "/menus reload <name> | /menu load <path> | /menu list | /menu open <name>"
+)
+@Permissible("menus.command.menu")
+public class MenuCommand {
+
+    @CommandProcessor
+    protected void run(@Sender ICommandSender sender, String[] args) {
+        sender.sendMessage(new TextComponentString("/menus reload <name> | /menu load <path> | /menu list | /menu open <name>"));
+    }
+}
