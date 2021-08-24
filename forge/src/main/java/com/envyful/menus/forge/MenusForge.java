9100b7601c62d5bc62bbaa9f7c431073f7a7fe42
danorris709 <dnorris443@gmail.com>
2021-08-24 09:55:11 +0100

    Feat: started main class

---
+++ forge/src/main/java/com/envyful/menus/forge/MenusForge.java
@@ -0,0 +1,55 @@
+package com.envyful.menus.forge;
+
+import com.envyful.api.forge.command.ForgeCommandFactory;
+import com.envyful.api.forge.concurrency.ForgeUpdateBuilder;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import org.bstats.forge.Metrics;
+
+import java.nio.file.Paths;
+
+@Mod(
+        modid = "menus",
+        name = "Menus Forge",
+        version = MenusForge.VERSION,
+        acceptableRemoteVersions = "*"
+)
+public class MenusForge {
+
+    protected static final String VERSION = "0.1.0";
+
+    private static MenusForge instance;
+
+    private ForgeCommandFactory commandFactory = new ForgeCommandFactory();
+
+    @Mod.EventHandler
+    public void onServerStarting(FMLPreInitializationEvent event) {
+        instance = this;
+
+        Metrics metrics = new Metrics(
+                Loader.instance().activeModContainer(),
+                event.getModLog(),
+                Paths.get("config/"),
+                12565 //TODO:
+        );
+
+        ForgeUpdateBuilder.instance()
+                .name("ForgeMenus")
+                .requiredPermission("menus.update.notify")
+                .owner("Pixelmon-Development")
+                .repo("ForgeMenus")
+                .version(VERSION)
+                .start();
+    }
+
+    @Mod.EventHandler
+    public void onServerStarting(FMLServerStartingEvent event) {
+
+    }
+
+    public static MenusForge getInstance() {
+        return instance;
+    }
+}
