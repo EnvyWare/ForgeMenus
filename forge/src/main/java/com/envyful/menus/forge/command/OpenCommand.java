7c40d320eb3a38043500dd20b130494d3b6f9055
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-09-29 15:52:51 +0100

    feat(forge): implement tab completions
    skip-ci

--- forge/src/main/java/com/envyful/menus/forge/command/OpenCommand.java
+++ forge/src/main/java/com/envyful/menus/forge/command/OpenCommand.java
@@ -3,11 +3,14 @@ package com.envyful.menus.forge.command;
 import com.envyful.api.command.annotate.Child;
 import com.envyful.api.command.annotate.Command;
 import com.envyful.api.command.annotate.Permissible;
+import com.envyful.api.command.annotate.executor.Argument;
 import com.envyful.api.command.annotate.executor.CommandProcessor;
+import com.envyful.api.command.annotate.executor.Completable;
 import com.envyful.api.command.annotate.executor.Sender;
 import com.envyful.api.forge.chat.UtilChatColour;
 import com.envyful.menus.forge.MenusForge;
 import com.envyful.menus.forge.data.Menu;
+import com.envyful.menus.forge.data.MenuTabCompleter;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.server.permission.PermissionAPI;
@@ -21,19 +24,7 @@ import net.minecraftforge.server.permission.PermissionAPI;
 public class OpenCommand {
 
     @CommandProcessor
-    public void run(@Sender EntityPlayerMP sender, String[] args) {
-        if (args.length != 1) {
-            sender.sendMessage(new TextComponentString("Opens the gui - /menus open <name>"));
-            return;
-        }
-
-        Menu menu = MenusForge.getInstance().getMenu(args[0]);
-
-        if(menu == null) {
-            sender.sendMessage(new TextComponentString("Menu doesn't exist!"));
-            return;
-        }
-
+    public void run(@Sender EntityPlayerMP sender, @Completable(MenuTabCompleter.class) @Argument Menu menu) {
         if (!PermissionAPI.hasPermission(sender, menu.getPermission())
                 && !sender.canUseCommand(4, menu.getPermission())) {
             sender.sendMessage(new TextComponentString(UtilChatColour.translateColourCodes('&',
