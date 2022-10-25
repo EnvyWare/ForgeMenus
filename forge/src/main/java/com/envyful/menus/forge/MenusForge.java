a9c9523c7d41fa245bbb1928fb5b8663411acb49
Daniel Norris <dnorris443@gmail.com>
2022-10-25 07:37:19 +0100

    chore(gradle): bump version to 2.0.7

--- forge/src/main/java/com/envyful/menus/forge/MenusForge.java
+++ forge/src/main/java/com/envyful/menus/forge/MenusForge.java
@@ -36,7 +36,7 @@ import java.util.Map;
 public class MenusForge {
 
     protected static final String MOD_ID = "menus";
-    protected static final String VERSION = "2.0.6";
+    protected static final String VERSION = "2.0.7";
 
     @Mod.Instance(MOD_ID)
     private static MenusForge instance;
